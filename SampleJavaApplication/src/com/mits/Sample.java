ee6b4443caebc298e5fe6432fe33a00435dc4c88
Naresh <Naresh@MITSCPU162>
2016-02-25 11:16:28 +0530

    Adding Sample Java Program

---
+++ SampleJavaApplication/src/com/mits/Sample.java
@@ -0,0 +1,8 @@
+package com.mits;
+
+public class Sample {
+
+	public void methodOne() {
+		System.out.println("We are in MethodOne");
+	}
+}
--- SampleJavaApplication/src/com/mits/Test.java
+++ SampleJavaApplication/src/com/mits/Test.java
@@ -7,6 +7,8 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Good morning");
+		Sample sampleObject = new Sample();
+		sampleObject.methodOne();
 	}
 
 }
